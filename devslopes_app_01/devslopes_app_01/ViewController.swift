0d1b630cbfbf6fc5c7e99a4ebc908223643998cf
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-07-28 07:40:19 +0100

    Initial

---
+++ devslopes_app_01/devslopes_app_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes_app_01
+//
+//  Created by Benjamin on 28/07/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
