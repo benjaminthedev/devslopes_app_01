7494ab8db21e484fc5e6c7586424730375506a15
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-07-31 15:15:00 +0100

    more edits

--- devslopes_app_01/devslopes_app_01/ViewController.swift
+++ devslopes_app_01/devslopes_app_01/ViewController.swift
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
         player.play()
         
         UIView.animate(withDuration: 2.3, animations: { 
-            self.rocket.frame = CGRect(x: 0, y: 20, width: 375, height: 402)
+            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
         }) { (finished) in
             self.programmingLbl.isHidden = false
             self.onLbl.isHidden = false
