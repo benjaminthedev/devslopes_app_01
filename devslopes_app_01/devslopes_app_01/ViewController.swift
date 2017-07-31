54c1daa745f213c63f676899ace251c8b41d2f5b
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-07-31 14:40:09 +0100

    Added functions etc

--- devslopes_app_01/devslopes_app_01/ViewController.swift
+++ devslopes_app_01/devslopes_app_01/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
 
+    @IBOutlet weak var powerButton: UIButton!
+    
+    @IBOutlet weak var cloudHolder: UIView!
+    
+    @IBOutlet weak var rocket: UIImageView!
+    
+    @IBOutlet weak var programmingLbl: UILabel!
+    
+    @IBOutlet weak var onLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerButton.isHidden = true
+        
+        
     }
-
-
 }
 
